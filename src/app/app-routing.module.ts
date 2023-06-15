f1fb6f288005ed749764442d1465cb87fd890163
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-15 13:43:27 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'modules', loadChildren: () => import('./modules/modules.module').then(m => m.ModulesModule)},
+  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule),canActivate: [AuthenticationGuard]},
 
-  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)},
-
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-
-  { path: 'test', loadChildren: () => import('./test/test.module').then(m => m.TestModule)},
-
-  { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule)},
+  { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule),canActivate: [AuthenticationGuard]},
 ];
 
 @NgModule({
