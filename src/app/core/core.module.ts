ba8311fd5aae4036c980cbb4fa4cdef7d6900fbb
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-15 13:43:36 +0530

    Initial commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,22 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { NotesComponent } from './notes/notes.component';
-import { CommentsComponent } from './comments/comments.component';
-import { ShareComponent } from './share/share.component';
-import { ActualDataDashBoardComponent } from './actual-data-dash-board/actual-data-dash-board.component';
-import { BaseLineListFinalComponent } from './base-line-list-final/base-line-list-final.component';
-import { BaselineCreationOrComponent } from './baseline-creation-or/baseline-creation-or.component';
+import { CreateBaseComponent } from './create-base/create-base.component';
 
 
 @NgModule({
   declarations: [
-    NotesComponent,
-    CommentsComponent,
-    ShareComponent,
-    ActualDataDashBoardComponent,
-    BaseLineListFinalComponent,
-    BaselineCreationOrComponent
+    CreateBaseComponent
   ],
   imports: [
     CommonModule,
