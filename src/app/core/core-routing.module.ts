b859e57080c2ec0cfffa538292d0b1dd3312e89e
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-15 13:43:34 +0530

    Initial commit

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,12 +1,9 @@
-import { BaseLineListFinalComponent } from './base-line-list-final/base-line-list-final.component';
-import { ActualDataDashBoardComponent } from './actual-data-dash-board/actual-data-dash-board.component';
+import { CreateBaseComponent } from './create-base/create-base.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'actual-data-dash-board', component: ActualDataDashBoardComponent },
-
-  { path: 'base-line-list-final', component: BaseLineListFinalComponent },
+  { path: 'create-base', component: CreateBaseComponent },
 ];
 
 @NgModule({
