63fe9b39e948c72ce90d34cda41c170b9b2570a2
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-15 13:43:41 +0530

    Initial commit

--- src/app/share/share-routing.module.ts
+++ src/app/share/share-routing.module.ts
@@ -1,7 +1,10 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
